b2c9a05a7ece4f6818120f5cf6ca20379013b04c
ikuchan <storyseller1980@gmail.com>
2019-01-24 17:41:35 +0900

    IOUTransferFlowTest Complete

--- kotlin-source/src/main/kotlin/net/corda/training/flow/IOUTransferFlow.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/flow/IOUTransferFlow.kt
@@ -1,6 +1,7 @@
 package net.corda.training.flow
 
 import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.contracts.Command
 import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.contracts.requireThat
 import net.corda.core.flows.CollectSignaturesFlow
@@ -12,9 +13,13 @@ import net.corda.core.flows.InitiatingFlow
 import net.corda.core.flows.SignTransactionFlow
 import net.corda.core.flows.StartableByRPC
 import net.corda.core.identity.Party
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.QueryCriteria
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
+import net.corda.training.contract.IOUContract
 import net.corda.training.state.IOUState
+import java.lang.IllegalArgumentException
 
 /**
  * This is the flow which handles transfers of existing IOUs on the ledger.
@@ -28,9 +33,39 @@ class IOUTransferFlow(val linearId: UniqueIdentifier, val newLender: Party): Flo
     @Suspendable
     override fun call(): SignedTransaction {
         // Placeholder code to avoid type error when running the tests. Remove before starting the flow task!
-        return serviceHub.signInitialTransaction(
-                TransactionBuilder(notary = null)
-        )
+
+        //get inputState(IOUState) queried by linearID
+        val queryCriteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(linearId))
+        val iouStateAndRef = serviceHub.vaultService.queryBy<IOUState>(queryCriteria).states.single()
+        val inputIouState = iouStateAndRef.state.data
+
+        //Owner of inputIou has to be this node
+        if(ourIdentity != inputIouState.lender){
+            throw IllegalArgumentException("IOU transfer can only be initiated by the IOU lender.")
+        }
+
+        //ceate OutputIou
+        val outputIouState = inputIouState.withNewLender(newLender = newLender)
+
+
+
+        //create transfer command
+
+
+        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+        val signers = (inputIouState.participants + newLender).map{it.owningKey}
+        val transfarCommand = Command(IOUContract.Commands.Transfer(),signers)
+        val builder = TransactionBuilder(notary = notary)
+        builder.addInputState(iouStateAndRef)
+        builder.addOutputState(outputIouState,IOUContract.IOU_CONTRACT_ID)
+        builder.addCommand(transfarCommand)
+        builder.verify(serviceHub)
+        val partialSignedTx = serviceHub.signInitialTransaction(builder)
+        val sessions = (inputIouState.participants + newLender - ourIdentity).map { initiateFlow(it)}.toSet()
+        val FullsignedTx = subFlow(CollectSignaturesFlow(partialSignedTx,sessions))
+
+        return subFlow(FinalityFlow(FullsignedTx))
+
     }
 }
 
@@ -48,7 +83,6 @@ class IOUTransferFlowResponder(val flowSession: FlowSession): FlowLogic<Unit>() 
                 "This must be an IOU transaction" using (output is IOUState)
             }
         }
-
         subFlow(signedTransactionFlow)
     }
 }
